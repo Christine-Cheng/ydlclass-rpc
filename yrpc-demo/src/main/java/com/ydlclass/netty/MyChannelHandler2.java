c202dd25fcd048b7412c9bc12873c7462660f727
Happy <atom164318791856@gmail.com>
2024-01-12 15:12:12 +0800

    修改

--- yrpc-demo/src/main/java/com/ydlclass/netty/MyChannelHandler2.java
+++ yrpc-demo/src/main/java/com/ydlclass/netty/MyChannelHandler2.java
@@ -11,19 +11,20 @@ import java.nio.charset.StandardCharsets;
  * @createTime 2023-06-11
  */
 public class MyChannelHandler2 extends ChannelInboundHandlerAdapter {
-
+    
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf byteBuf = (ByteBuf)msg;
+        ByteBuf byteBuf = (ByteBuf) msg;
         System.out.println("客户端已经收到了消息：-->" + byteBuf.toString(StandardCharsets.UTF_8));
         // 可以通过ctx获取channel
     }
+    
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
-    {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         //出现异常的时候执行的动作（打印并关闭通道）
         cause.printStackTrace();
         ctx.close();
     }
-
+    
+    
 }
